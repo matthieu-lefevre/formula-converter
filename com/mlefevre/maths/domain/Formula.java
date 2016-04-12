c81836cca70a2836285e4b41e1aa7543f9ad417c
Matthieu LEFEVRE <lefevre.matthieu@outlook.com>
2016-04-12 19:24:08 +0200

    Create Formula.java

---
+++ com/mlefevre/maths/domain/Formula.java
@@ -0,0 +1,14 @@
+com.mlefevre.maths.domain;
+
+public class Formula {
+
+    private FormulaElements elements;
+
+    public FormulaElements getElements() {
+        return elements;
+    }
+
+    public void setElements(FormulaElements elements) {
+        this.elements = elements;
+    }
+}
