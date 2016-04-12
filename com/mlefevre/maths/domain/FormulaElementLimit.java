9a4f618a91cbcf20691cce4c1e9eaad462907f72
Matthieu LEFEVRE <lefevre.matthieu@outlook.com>
2016-04-12 19:26:06 +0200

    Create FormulaElementLimit.java

---
+++ com/mlefevre/maths/domain/FormulaElementLimit.java
@@ -0,0 +1,25 @@
+package com.mlefevre.maths.domain;
+
+import com.sgcib.fip.common.maths.domain.enumeration.FormulaElementLimitType;
+
+public class FormulaElementLimit {
+
+    private FormulaElementLimitType type;
+    private String value;
+
+    public FormulaElementLimitType getType() {
+        return type;
+    }
+
+    public void setType(FormulaElementLimitType type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
